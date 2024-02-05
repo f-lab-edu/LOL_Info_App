85bc6acb90ca827635c3378d0a379f6975f09824
DeokHo98 <ekdj98@gmail.com>
2024-02-05 22:11:41 +0900

    프로젝트 재 생성

---
+++ LOLInfoApp/LOLInfoApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LOLInfoApp
+//
+//  Created by 정덕호 on 2024/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
