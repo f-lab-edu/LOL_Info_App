9281ef43c8c03e6cd8537f8d7fd2b11615925706
JeongDeokho <deokhotion@gmailc.om>
2024-02-15 14:50:00 +0900

    feat: 네트워크 관련 OSLog Extension 구현

---
+++ LOLInfoApp/LOLInfoApp/Commons/Extension/OSLog.swift
@@ -0,0 +1,23 @@
+//
+//  OSLogEx.swift
+//  LOLInfoApp
+//
+//  Created by Jeong Deokho on 2024/02/15.
+//
+
+import Foundation
+import OSLog
+
+extension OSLog {
+    
+    private static var subsystem = Bundle.main.bundleIdentifier ?? ""
+    
+    // MARK: - Network
+    
+    static let network = OSLog(subsystem: subsystem, category: "Network")
+    static let networkError = OSLog(
+        subsystem: subsystem,
+        category: "NetwrokError"
+    )
+    
+}
