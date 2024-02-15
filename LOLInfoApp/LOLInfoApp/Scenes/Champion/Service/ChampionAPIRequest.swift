b7c67fb6223a784405531cd0fac4c6b47e7a2d8b
JeongDeokho <deokhotion@gmailc.om>
2024-02-15 18:29:23 +0900

    feat: ChampionAPIRequest 구현

---
+++ LOLInfoApp/LOLInfoApp/Scenes/Champion/Service/ChampionAPIRequest.swift
@@ -0,0 +1,42 @@
+//
+//  ChampionAPIRequest.swift
+//  LOLInfoApp
+//
+//  Created by Jeong Deokho on 2024/02/15.
+//
+
+import Foundation
+import Alamofire
+
+enum ChampionAPIRequest: Requestable {
+    // MARK: - Case
+    
+    case main
+    
+    // MARK: - Path
+    
+    var path: String {
+        switch self {
+        case .main:
+            return "/data/ko_KR/champion.json"
+        }
+    }
+    
+    // MARK: - URL
+    
+    var url: String {
+        switch self {
+        case .main:
+            return NetworkURLStringCollection.baseURLString + path
+        }
+    }
+    
+    // MARK: - HTTPMethod
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .main:
+            return .get
+        }
+    }
+}
