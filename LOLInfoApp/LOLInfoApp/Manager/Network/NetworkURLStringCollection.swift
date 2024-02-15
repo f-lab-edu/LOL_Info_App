3fb0260092807c015006fce39661f30936752d0b
JeongDeokho <deokhotion@gmailc.om>
2024-02-15 13:57:17 +0900

    feat: NetworkURLStringCollection 구현
    네트워크에서 사용하는 URLString을 모아둘 객체입니다.

---
+++ LOLInfoApp/LOLInfoApp/Manager/Network/NetworkURLStringCollection.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkURLStringCollection.swift
+//  LOLInfoApp
+//
+//  Created by Jeong Deokho on 2024/02/15.
+//
+
+import Foundation
+
+struct NetworkURLStringCollection {
+    static let baseURLString = "https://ddragon.leagueoflegends.com/cdn/14.2.1"
+}
