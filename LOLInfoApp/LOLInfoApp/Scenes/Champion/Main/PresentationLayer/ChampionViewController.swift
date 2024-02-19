cc25f0d09a16419c389b5a58560ad6186d1fda35
DeokHo98 <ekdj98@gmail.com>
2024-02-19 20:35:47 +0900

    chore: 상속을 안하는 class에 final 키워드 추가

--- LOLInfoApp/LOLInfoApp/Scenes/Champion/Main/PresentationLayer/ChampionViewController.swift
+++ LOLInfoApp/LOLInfoApp/Scenes/Champion/Main/PresentationLayer/ChampionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ChampionViewController: UIViewController {
+final class ChampionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
