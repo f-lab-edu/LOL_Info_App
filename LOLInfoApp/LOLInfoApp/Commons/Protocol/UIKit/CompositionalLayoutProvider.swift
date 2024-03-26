5bdbc5158dd7b242093a650acffcaf6fee94e996
JeongDeokHo <93653997+DeokHo98@users.noreply.github.com>
2024-03-26 13:40:00 +0900

    Feautre/automation swiftlint and slacknotification (#16)

---
+++ LOLInfoApp/LOLInfoApp/Commons/Protocol/UIKit/CompositionalLayoutProvider.swift
@@ -0,0 +1,21 @@
+//
+//  CompositionalLayoutProvider.swift
+//  LOLInfoApp
+//
+//  Created by Jeong Deokho on 2024/03/08.
+//
+
+import UIKit
+
+protocol CompositionalLayoutProvider {
+
+    /// Layout Group을 만들때 필요한 Item을 생성하는 함수입니다.
+    func getLayoutItem() -> NSCollectionLayoutItem
+
+    /// Layout Section을 만들때 필요한 Gruop을 생성하는 함수입니다.
+    func getLayoutGroup() -> NSCollectionLayoutGroup
+
+    /// Compositional Layout을 만들때 필요한 Section을 생성하는 함수입니다.
+    func getLayoutSection() -> NSCollectionLayoutSection
+
+}
--- LOLInfoApp/LOLInfoApp/Scenes/Champion/APIEndPoint/ChampionAPIEndpoint.swift
+++ LOLInfoApp/LOLInfoApp/Scenes/Champion/APIEndPoint/ChampionAPIEndpoint.swift
@@ -10,31 +10,31 @@ import Foundation
 import Alamofire
 
 enum ChampionAPIEndpoint: Requestable {
-    
+
     // MARK: - Case
-    
+
     case main
-    
+
     // MARK: - Path
-    
+
     var path: String {
         switch self {
         case .main:
             return Bundle.getChampionPathString(key: .main)
         }
     }
-    
+
     // MARK: - URL
-    
+
     var url: String {
         switch self {
         case .main:
             return Bundle.getURLString(key: .baseURL) + path
         }
     }
-    
+
     // MARK: - HTTPMethod
-    
+
     var httpMethod: HTTPMethod {
         switch self {
         case .main:
