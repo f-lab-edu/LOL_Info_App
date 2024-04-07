fbcf6d6a8e9cb665849865a8d0656b1094ea744d
JeongDeokHo <93653997+DeokHo98@users.noreply.github.com>
2024-04-08 08:20:52 +0900

    refactor: datalayer 리팩토링 (#18)

--- LOLInfoApp/LOLInfoApp/Scenes/Champion/Main/DataLayer/ChampionMainRepository.swift
+++ LOLInfoApp/LOLInfoApp/Scenes/Champion/Main/DataLayer/ChampionMainRepository.swift
@@ -5,18 +5,39 @@
 //  Created by 정덕호 on 2024/02/22.
 //
 
+import Combine
 import Foundation
 
-import Combine
+import Alamofire
+
+// MARK: - EndPoint
+
+struct ChampionMainRepository: Requestable {
+
+    var path: String {
+        return Bundle.getChampionPathString(key: .main)
+    }
+
+    var url: String {
+            return Bundle.getURLString(key: .baseURL) + path
+    }
+
+    var httpMethod: HTTPMethod {
+            return .get
+    }
+}
+
+// MARK: - Dependency
 
 protocol ChampionMainRepositoryDependency {
     func requestAllChampionList() -> AnyPublisher<ChampionMainListEntity, Error>
 }
 
-struct ChampionMainRepository: ChampionMainRepositoryDependency {
+// MARK: - Request Function
+
+extension ChampionMainRepository: ChampionMainRepositoryDependency {
     func requestAllChampionList() -> AnyPublisher<ChampionMainListEntity, Error> {
-        let publisher: AnyPublisher<ChampionMainListDTO, Error> =
-        ChampionAPIEndpoint.main.requestData()
+        let publisher: AnyPublisher<ChampionMainListDTO, Error> = requestData()
         return publisher
             .map { $0.toEntity() }
             .eraseToAnyPublisher()
